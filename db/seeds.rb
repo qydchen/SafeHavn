48c9a2eefac6ba7e1a9a4f52b94791b631c52c05
qydchen <qydchen@gmail.com>
2017-06-22 16:40:13 -0400

    heroku seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create({"email": "guest@live.com", "first": "", "last": "", "password": "asdf1234", "month": "", "day": "", "year": ""})
+User.create({"email": "guest@live.com", "first": "Ronald", "last": "McDonald", "password": "asdf1234", "month": "5", "day": "11", "year": "1980"})
