237d4dd4bfb19eb1a4a4a5ad513aa6929e3f5fd4
qydchen <qydchen@gmail.com>
2017-06-26 14:19:20 -0400

    attempt to fix aws

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -1,35 +1,35 @@
-// export default class MarkerManager {
-//   constructor(map) {
-//     this.map = map;
-//     this.markers = {};
-//   }
-//
-//   updateMarkers(benches) {
-//     let markers = this.markers;
-//     benches.forEach(bench => {
-//       markers[bench.id] = this.createMarkerFromBench(bench);
-//     });
-//
-//   }
-//
-//   createMarkerFromBench(bench) {
-//     const lat = bench.lat;
-//     const lng = bench.lng;
-//     let marker = new google.maps.Marker({
-//       position: {lat, lng},
-//       title: bench.description,
-//       animation: google.maps.Animation.DROP,
-//       map: this.map
-//     });
-//     marker.addListener('click', this.toggleBounce.bind(marker));
-//   }
-//
-//   toggleBounce() {
-//     if (this.getAnimation() !== null) {
-//       this.setAnimation(null);
-//     } else {
-//       this.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-//   }
-//
-// }
+export default class MarkerManager {
+  constructor(map) {
+    this.map = map;
+    this.markers = {};
+  }
+
+  updateMarkers(benches) {
+    let markers = this.markers;
+    benches.forEach(bench => {
+      markers[bench.id] = this.createMarkerFromBench(bench);
+    });
+
+  }
+
+  createMarkerFromBench(bench) {
+    const lat = bench.lat;
+    const lng = bench.lng;
+    let marker = new google.maps.Marker({
+      position: {lat, lng},
+      title: bench.description,
+      animation: google.maps.Animation.DROP,
+      map: this.map
+    });
+    marker.addListener('click', this.toggleBounce.bind(marker));
+  }
+
+  toggleBounce() {
+    if (this.getAnimation() !== null) {
+      this.setAnimation(null);
+    } else {
+      this.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+
+}
