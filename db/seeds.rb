1f8a62fe3a1c33612c1e9f36e236456bde0daa5b
qydchen <qydchen@gmail.com>
2017-06-25 23:46:16 -0400

    create more seed data, attach router links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,78 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def host_id
+  (rand(1..2))
+end
+
+def lat
+  (rand(0...70) - 140 + rand())
+end
+
+def lng
+  (rand(0...180) - 180 + rand())
+end
+
+def title
+  (Faker::Pokemon.location)
+end
+
+def price
+  (rand(0..500))
+end
+
+def address
+   (Faker::Address.street_address + ", " + Faker::Address.city + ", " + Faker::Address.country)
+end
+
+def bathrooms
+  (rand(0..5)
+end
+
+def bedrooms
+  (rand(0..6))
+end
+
+def max_guests
+  (rand(0..12))
+end
+
+def beds
+  (rand(0..8))
+end
+
+def property_type
+  "House"
+end
+
+def room_type
+  "Entire home/apt"
+end
+
+def internet
+  [true, false].sample
+end
+
+def family
+  [true, false].sample
+end
+
+def parking
+  [true, false].sample
+end
+
+def kitchen
+  [true, false].sample
+end
+
+def description
+  Faker::Lorem.paragraph(2)
+end
+
+def cancellation
+  (["Flexible", "Moderate", "Strict"].sample)
+end
+
 User.create({"email": "guest@live.com",
   "first": "Ronald",
   "last": "McDonald",
@@ -60,7 +132,7 @@ Home.create({"host_id": 2,
   "family": false,
   "parking": true,
   "kitchen": false,
-  "description": "test2 I like cake and bunch of text this is a really long set state",
+  "description": Faker::Lorem.paragraph(2),
   "cancellation": "Moderate",
   "image": File.open('app/assets/images/houses/002.jpeg')
   })
@@ -81,7 +153,7 @@ Home.create({"host_id": 2,
   "family": true,
   "parking": false,
   "kitchen": true,
-  "description": "test3 bunch of text",
+  "description": Faker::Lorem.paragraph(2),
   "cancellation": "Strict",
   "image": File.open('app/assets/images/houses/003.jpeg')
   })
@@ -102,7 +174,196 @@ Home.create({"host_id": 2,
   "family": false,
   "parking": false,
   "kitchen": false,
-  "description": "test4 bunch of text",
+  "description": Faker::Lorem.paragraph(2),
   "cancellation": "Flexible",
   "image": File.open('app/assets/images/houses/004.jpeg')
   })
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/flowers-garden-house-165884.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/wood-landscape-summer-161041.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/light-cars-coffee-286483.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/house-plant-table-271795.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/wood-landscape-summer-161041.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/wood-landscape-summer-197758.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/building-house-roof-186077.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/snow-winter-mountain-91998.jpeg"
+})
+
+Home.create({"host_id": host_id,
+  "lat": lat,
+  "lng": lng,
+  "title": title,
+  "price": price,
+  "address": address,
+  "bathrooms": bathrooms,
+  "bedrooms": bedrooms,
+  "max_guests": max_guests,
+  "beds": beds,
+  "property_type": property_type,
+  "room_type": room_type,
+  "internet": internet,
+  "family": family,
+  "parking": parking,
+  "kitchen": kitchen,
+  "description": description,
+  "cancellation": cancellation,
+  "image": "https://s3.amazonaws.com/safehavns-dev/homes/images/000/000/004/original/sky-architecture-castle-99635.jpeg"
+})
