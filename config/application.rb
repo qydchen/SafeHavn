919409f24ff265bc500d80080179257a8a5bc1f8
qydchen <qydchen@gmail.com>
2017-06-26 12:20:39 -0400

    aws stuff

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,13 @@ module SafeHavnApp
 
     config.paperclip_defaults = {
       :storage => :s3,
-      :s3_region => ENV["us-east-2"],
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
+        s3_host_name: "s3-#{ENV['s3_region']}.amazonaws.com",
         :s3_region => ENV["s3_region"],
-        :s3_host_name => "s3-us-east-2.amazonaws.com"
+        :url => ":s3_host_name"
       }
     }
 
