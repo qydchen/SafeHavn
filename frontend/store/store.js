1c0b8df77b229d1c61387c7e2d06ebb31da39cf7
qydchen <qydchen@gmail.com>
2017-07-03 21:42:11 -0400

    remove redux logger from production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
+const middlewares = [thunk];
 
 import RootReducer from '../reducers/root_reducer';
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { createLogger } = require('redux-logger');
+  middlewares.push(createLogger());
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
