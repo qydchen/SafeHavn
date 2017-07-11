de19339893204f6a70b5ef25c122a4f641b07b72
qydchen <qydchen@gmail.com>
2017-07-10 20:50:59 -0400

    store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,7 +6,7 @@ const middlewares = [thunk];
 import RootReducer from '../reducers/root_reducer';
 
 if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
+  debugger;
   const createLogger = require('redux-logger');
   middlewares.push(createLogger());
 }
