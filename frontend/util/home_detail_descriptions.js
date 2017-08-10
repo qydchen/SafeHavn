d691fa15a44ffbd894fae7fc75f004dc7fdcf552
qydchen <qydchen@gmail.com>
2017-08-10 12:35:30 -0400

    make util file to DRY descriptions for creating and showing homes

---
+++ frontend/util/home_detail_descriptions.js
@@ -0,0 +1,21 @@
+export const cancellationText = {
+  Strict: "Cancel up to 7 days before your trip and get a 50% refund plus service fees back.",
+  Moderate: "Cancel up to 5 days before your trip and get a full refund, including service fees.",
+  Flexible: "Cancel up to 24 hours before your trip and get a full refund, including service fees."
+}
+
+export const spaceText = {
+  max_guests: "Accommodates",
+  bathrooms: "Bathrooms",
+  bedrooms: "Bedrooms",
+  beds: "Beds",
+  property_type: "Property Type",
+  room_type: "Room Type"
+}
+
+export const amenityText = {
+  internet: ["Internet", "wifi-img"],
+  family: ["Family/kid friendly", "family-img"],
+  parking: ["Free parking on premises", "parking-img"],
+  kitchen: ["Kitchen", "cutlery-img"],
+}
