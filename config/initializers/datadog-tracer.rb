42d07ef31807c3f81aff3755e4bd888317f0ea9b
qydchen <qydchen@gmail.com>
2017-12-10 01:46:00 -0500

    datadog-tracer

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,17 +1,5 @@
 Rails.configuration.datadog_trace = {
-  enabled: true,
-  auto_instrument: false,
-  auto_instrument_redis: false,
-  auto_instrument_grape: false,
-  default_service: 'rails-app',
-  default_controller_service: 'rails-controller',
-  default_cache_service: 'rails-cache',
-  default_database_service: 'postgresql',
-  template_base_path: 'views/',
-  tracer: Datadog.tracer,
-  debug: false,
-  trace_agent_hostname: 'localhost',
-  trace_agent_port: 8126,
-  env: nil,
-  tags: {}
+  auto_instrument: true,
+  auto_instrument_redis: true,
+  default_service: 'my-rails-app'
 }
