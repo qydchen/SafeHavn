c32ba980a0d22eb1e0bd3cc71c74068b0bc1de89
qydchen <qydchen@gmail.com>
2018-03-13 19:59:04 -0400

    refactor time complexity of marker manager

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -7,53 +7,41 @@ export default class MarkerManager {
 
   updateMarkers(homes) {
     const homesObj = {};
-    homes.forEach(home => homesObj[home.id] = home);
 
-    homes
-      .filter(home => !this.markers[home.id])
-      .forEach(newHome => { this.createMarkerFromHome(newHome, this.handleClick);
+    homes.forEach(home => {
+      homesObj[home.id] = home;
+      if (!this.markers[home.id]) this.createMarkerFromHome(home, this.handleClick);
     });
 
-    Object.keys(this.markers)
-      .filter(homeid => !homesObj[homeid])
-      .forEach((homeid) => this.removeMarker(this.markers[homeid]))
+    for (let key in this.markers) {
+      let { id } = this.markers[key];
+      if (!homesObj[id]) this.removeMarker(this.markers[id]);
+    }
   }
 
   createMarkerFromHome(home) {
-    const image = "https://s3.amazonaws.com/safehavns-dev/mark.png";
-    const lat = home.lat;
-    const lng = home.lng;
+    const {lat, lng, price, id} = home;
     let marker = new google.maps.Marker({
       position: {lat, lng},
       label: {
         color: "#ffffff",
         fontFamily: "Helvetica",
-        text: "$"+String(home.price),
+        text: "$"+String(price),
         fontSize: "14.5px",
         fontWeight: "700",
       },
-      icon: image,
+      icon: "https://s3.amazonaws.com/safehavns-dev/mark.png",
       animation: google.maps.Animation.DROP,
       map: this.map,
-      homeid: home.id
+      id
     });
-    marker.addListener('click', this.toggleBounce.bind(marker));
-
     marker.addListener('click', () => this.handleClick(home));
-    this.markers[marker.homeid] = marker;
+    this.markers[marker.id] = marker;
   }
 
   removeMarker(marker) {
-    this.markers[marker.homeid].setMap(null);
-    delete this.markers[marker.homeid];
-  }
-
-  toggleBounce() {
-    if (this.getAnimation() !== null) {
-      this.setAnimation(null);
-    } else {
-      this.setAnimation(google.maps.Animation.BOUNCE);
-    }
+    this.markers[marker.id].setMap(null);
+    delete this.markers[marker.id];
   }
 
 }
